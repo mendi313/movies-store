c4d9d3373d3dfb57461e32e912831c794c9c005b
mendi weissman <770mmw@gmail.com>
2023-12-20 10:53:48 +0200

    allow cors

--- next.config.js
+++ next.config.js
@@ -17,7 +17,7 @@ const nextConfig = {
         source: '/api/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: `${process.env.NEXT_PUBLIC_APP_URL}` }, // replace this your actual origin
+          { key: 'Access-Control-Allow-Origin', value: '*' }, 
           { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
           {
             key: 'Access-Control-Allow-Headers',
